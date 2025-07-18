d684fff27b954faea1ea9f6e606bdb3bfedf4336
Frimpong <executivekfb27@gmail.com>
2025-07-18 23:30:49 +0000

    add the home page text and layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div className={styles.page}>
-     <h1>Hello Ahonyah</h1>
+      <div className={styles.hero_container}>
+        <div className={styles.hero_text}>
+          <h1>Digitize Your Susu, <br/>Grow Your Savings Together.</h1>
+        
+          <p>Securely manage your traditional rotating savings groups online, <br/> with automated contributions and trusted community tools</p>
+        </div>
+
+        <div className={styles.cta_container}>
+          <button className={styles.cta}><Link href="./signup">Get started for Free</Link></button>
+        </div>
+          
+       
+        <p className={styles.small_print}>No hidden fees, Automated & Secure</p>
+      </div>
     </div>
   );
 }
